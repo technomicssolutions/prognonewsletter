8c50169d863de19588a3d826701e34a2bbd06720
Shefsi <shefsi@technomicssolutions.com>
2014-12-30 16:11:14 +0530

    test commit

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
+$db['default']['password'] = 'rtee';
 $db['default']['database'] = 'prognonewsletter';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
